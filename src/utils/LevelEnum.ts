8032b4d189d942be3f0e9177a39a2aabf535ab07
Xuan L <138820189+XnThanh@users.noreply.github.com>
2025-10-16 06:56:36 -0400

    LevelMap test complete

--- src/utils/LevelEnum.ts
+++ src/utils/LevelEnum.ts
@@ -1,6 +1,10 @@
-export enum LevelEnum {
-  Newbie = "Newbie",
-  Beginner = "Beginner",
-  Intermediate = "Intermediate",
-  Advanced = "Advanced",
-}
+import { ID } from "@utils/types.ts";
+
+export const LevelEnum = {
+  Newbie: "Newbie" as ID,
+  Beginner: "Beginner" as ID,
+  Intermediate: "Intermediate" as ID,
+  Advanced: "Advanced" as ID,
+} as const;
+
+export type LevelEnum = (typeof LevelEnum)[keyof typeof LevelEnum];
